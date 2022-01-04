15b9435b9e244c9a276fc2303e9c8d7b301d3d3e
Jelenkee <ichhabadhs@posteo.de>
2022-01-04 23:10:04 +0100

    cache1

--- index.js
+++ index.js
@@ -1,3 +1,132 @@
-module.exports = function cache(params) {
+const fs = require("fs-extra");
+const { join } = require("path");
+const { createHash } = require("crypto");
+const debounce = require("just-debounce");
 
+const UNIT_BYTE = "byte";
+const UNIT_FILE = "file";
+const REGISTRY = "4eadc5713cd7b0c7c0673d765edb0249f7295a72.json";
+
+class FileLRUCache {
+    constructor(options = {}) {
+        this.options = options;
+
+        if (typeof options.dir !== "string") {
+            throw new Error("option 'dir' is required");
+        }
+        this.dir = options.dir;
+
+        if (typeof options.maxSize !== "number") {
+            throw new Error("option 'maxSize' is required");
+        }
+        this.maxSize = options.maxSize;
+        this.maxSizeUnit = options.maxSizeUnit || UNIT_FILE;
+        this.registry = {};
+
+
+        this.bouncedRemoveOutdated = debounce(this._removeOutdated, 1000, true, true);
+
+        this._init().catch(console.error)
+        setInterval(() => {
+
+        }, 1000).unref();
+    }
+    async _init() {
+        await fs.ensureDir(this.dir);
+        this.options.clear && await this.clear();
+        await this._removeOutdated();
+        try {
+            this.registry = await fs.readJson(join(this.dir, REGISTRY));
+        } catch (error) {
+            if (error.message && error.message.startsWith("ENOENT")) {
+                return;
+            }
+            throw error;
+        }
+
+    }
+    async get(key) {
+        const hash = this._hashKey(key);
+        try {
+            return await fs.readFile(join(this.dir, hash))
+        } catch (error) {
+            if (error.message && error.message.startsWith("ENOENT")) {
+                return undefined;
+            }
+            throw error;
+        }
+    }
+    async has(key) {
+        const hash = this._hashKey(key);
+        return await fs.pathExists(join(this.dir, hash));
+    }
+    async set(key, value) {
+        const hash = this._hashKey(key);
+        this.registry[key] = hash;
+        await fs.writeFile(join(this.dir, hash), value)
+        await this._updateFileRegistry();
+    }
+    async del(key) {
+        const hash = this._hashKey(key);
+        await fs.unlink(join(this.dir, hash));
+    }
+    async size() {
+        return (await this._files()).length;
+    }
+    keys() {
+        return Object.keys(this.registry);
+    }
+    async values() {
+
+    }
+    async entries() {
+
+    }
+    async clear() {
+        await Promise.all((await this._files())
+            .map(file => console.log(file)|fs.unlink(file)));
+        await fs.unlink(join(this.dir, REGISTRY));
+    }
+    async _files() {
+        return await Promise.all((await fs.readdir(this.dir))
+            .filter(file => !file.endsWith(".json"))
+            .map(file => join(this.dir, file)));
+    }
+    async _removeOutdated() {
+        const files = (await this._files())
+            .map(async file => ({ file, stat: await fs.stat(file) }));
+        if (this.maxSizeUnit === UNIT_BYTE) {
+            const totalSize = files.map(f => f.stat.size).reduce((a, b) => a + b, 0);
+            if (totalSize > this.maxSize) {
+                files.sort((a, b) => a.stat.atimeMs - b.stat.atimeMs);
+                const over = total - this.maxSize;
+                let count = 0;
+                let filesToDelete = [];
+                for (const f of files) {
+                    filesToDelete.push(f.file);
+                    count += f.stat.size;
+                    if (count >= over) {
+                        break;
+                    }
+                }
+                await Promise.all(filesToDelete.map(f => unlink(f)));
+            }
+        } else {
+            if (files.length > this.maxSize) {
+                files.sort((a, b) => a.stat.atimeMs - b.stat.atimeMs);
+                await Promise.all(files.slice(0, this.length - this.maxSize)
+                    .map(f => fs.unlink(f)));
+            }
+        }
+    }
+    async _updateFileRegistry() {
+        await fs.writeJson(join(this.dir, REGISTRY), this.registry);
+    }
+
+    _hashKey(key) {
+        return createHash("sha1")
+            .update(key)
+            .digest().toString("hex");
+    }
 }
+module.exports = FileLRUCache;
--- test.js
+++ test.js
@@ -1,6 +1,52 @@
 const { test } = require("tap");
+const LRU = require("./index");
+const { mkdtemp } = require("fs/promises");
+const { tmpdir } = require("os");
+const { join } = require("path");
 
-test("true", t => {
-    t.plan(1);
-    t.ok(true);
-})
+test("get", async t => {
+    t.plan(2);
+
+    const lru = new LRU({
+        dir: await mkTmp(),
+        maxSize: 12
+    });
+    t.equal(await lru.get("test"), undefined);
+    await lru.set("test", "val");
+    t.same(await lru.get("test"), Buffer.from("val"));
+});
+
+test("clear", async t => {
+    t.plan(3);
+
+    const dir=await mkTmp();
+
+    const lru1 = new LRU({
+        dir: dir,
+        maxSize: 12
+    });
+    await lru1.set("test", "val");
+    await lru1.set("test2", "val");
+    t.equal(await lru1.size(),2);
+
+    const lru2 = new LRU({
+        dir: dir,
+        maxSize: 12
+    });
+    t.equal(await lru2.size(),2);
+
+    const lru3 = new LRU({
+        dir: dir,
+        maxSize: 12,
+        clear: true
+    });
+    console.log(dir);
+    //await lru3.clear();
+    setImmediate(async ()=>{
+        t.equal(await lru3.size(),0);
+    });
+});
+
+async function mkTmp() {
+    return await mkdtemp(join(tmpdir(), "tt-"));
+}
